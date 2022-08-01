4d22254541282b69fd475196ef004f4ce357487f
souminm <souminm@gmail.com>
2022-08-01 18:41:55 +0530

    Program1-Printing Patterns - INEURON

---
+++ src/PatternAssign1.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+public class PatternAssign1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        //1 Printing INEURON
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || i==n-1||j==n/2){
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("  ");
+            for(int j=0;j<n;j++){
+                if(j==0 || j==n-1||i==j){
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("  ");
+            for(int j=0;j<n;j++){
+                if(j==0 || i==0||i==n-1||i==n/2){
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("  ");
+            for(int j=0;j<n;j++){
+                if(j==0 || j==n-1||i==n-1){
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("  ");
+            for(int j=0;j<n;j++){
+                if(j==0 || i==0||i==n/2||i<=n/2 && j==n-1 ||i>n/2 &&j==i){
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("  ");
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0||j==n-1||i==n-1){
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+            for(int j=0;j<n;j++){
+                if(j==0 || j==n-1||i==j){
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
