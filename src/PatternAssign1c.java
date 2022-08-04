ba21397392045c9b65d6c1276f57906208d26f5c
souminm <souminm@gmail.com>
2022-08-04 22:20:07 +0530

    Program1-Printing Patterns - INEURON

--- src/PatternAssign1c.java
+++ src/PatternAssign1c.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
 public class PatternAssign1c {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i>(n-1)/2 && i-j>=n/2 || i>(n-1)/2 && j-i>=(n-1)/2||i==n-1||i>(n)/2 && i!=n-1 && j-i<=(n-2)/2)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
